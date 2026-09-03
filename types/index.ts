84bb3e589eb3f3d45b90144314010649bf777580
Cristian Lahoz <108961907+m415x@users.noreply.github.com>
2026-09-03 11:37:35 -0300

    chore: export training volume type

--- types/index.ts
+++ types/index.ts
@@ -21,6 +21,7 @@ export * from '@/types/training/intensity.types'
 export * from '@/types/training/periodization.types'
 export * from '@/types/training/workout.types'
 export * from '@/types/training/session.types'
+export * from '@/types/training/volume.types'
 export * from '@/types/training/workout-log.types'
 export * from '@/types/training/fitness-fatigue.types'
 
