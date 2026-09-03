315e037ddd390f8de2e370038bacbbf9afaa950b
Cristian Lahoz <108961907+m415x@users.noreply.github.com>
2026-09-03 10:32:32 -0300

    feat: add workout type to sessions

--- types/training/session.types.ts
+++ types/training/session.types.ts
@@ -1,10 +1,12 @@
 import type { BaseEntity } from '@/types/core/base.types'
+import type { WorkoutType } from '@/types/training/workout.types'
 
 export interface Session extends BaseEntity {
   teamId: string
   workoutId?: string | null
   date: string
   title: string
+  type: WorkoutType
   locationKey?: string | null
   trackPath?: string | null
   notes?: string | null
