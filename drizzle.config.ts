1ad6be6d1f55330623c3ecc13a3ccbaef9510baa
Cristian Lahoz <108961907+m415x@users.noreply.github.com>
2026-09-04 11:25:15 -0300

    chore: include load strategy schema in drizzle

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from 'drizzle-kit'
 
 export default defineConfig({
-  schema: './db/schema.ts',
+  schema: ['./db/schema.ts', './db/load-strategy-schema.ts'],
   out: './drizzle',
   dialect: 'sqlite',
   dbCredentials: {
