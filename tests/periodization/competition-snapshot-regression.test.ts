f8febc62a946d937c5e0c943817f2ab537b06c95
Cristian Lahoz <108961907+m415x@users.noreply.github.com>
2026-09-12 23:52:08 -0300

    test: include H12 schema in competition snapshot database

--- tests/periodization/competition-snapshot-regression.test.ts
+++ tests/periodization/competition-snapshot-regression.test.ts
@@ -8,6 +8,7 @@ import { drizzle } from 'drizzle-orm/better-sqlite3'
 import * as competitionEntrySchema from '@/db/competition-entry-schema'
 import * as intensityStrategySchema from '@/db/intensity-strategy-schema'
 import * as loadStrategySchema from '@/db/load-strategy-schema'
+import * as readinessSchema from '@/db/readiness-schema'
 import * as schema from '@/db/schema'
 import * as sessionGenerationPreferencesSchema from '@/db/session-generation-preferences-schema'
 import { macrocycles } from '@/db/schema'
@@ -175,6 +176,7 @@ function createTestDatabase() {
         ...intensityStrategySchema,
         ...sessionGenerationPreferencesSchema,
         ...competitionEntrySchema,
+        ...readinessSchema,
       },
     }),
   }
