4155ce348def1e7c5b9a5ff672a4217084586ec6
Cristian Lahoz <108961907+m415x@users.noreply.github.com>
2026-09-17 20:22:03 -0300

    feat(KAN-366): provide edition courses to lifecycle controls

--- app/[locale]/dashboard/competitions/[[...segments]]/page.tsx
+++ app/[locale]/dashboard/competitions/[[...segments]]/page.tsx
@@ -292,7 +292,7 @@ export default async function CompetitionsPage({ params, searchParams }: Props) 
           </CardContent>
         </Card>
 
-        <EditionRegistrations editionRegistrationGroups={editionRegistrationGroups} />
+        <EditionRegistrations editionRegistrationGroups={editionRegistrationGroups} availableCourses={courses} />
 
         <div className='flex flex-col gap-3 sm:flex-row sm:items-center sm:justify-between'>
           <div>
