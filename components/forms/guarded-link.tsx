28b3ca993ff32f78ae241deadde733b72c75ef31
Cristian Lahoz <108961907+m415x@users.noreply.github.com>
2026-09-18 12:46:54 -0300

    fix: align dirty guard with React and Next APIs

--- components/forms/guarded-link.tsx
+++ components/forms/guarded-link.tsx
@@ -1,24 +1,26 @@
 'use client'
 
 import Link, { type LinkProps } from 'next/link'
+import { useRouter } from 'next/navigation'
 import type { ComponentProps } from 'react'
 import { useDirtyFormGuard } from './dirty-form-guard'
 
 type GuardedLinkProps = LinkProps & Omit<ComponentProps<'a'>, keyof LinkProps>
 
-export function GuardedLink({ onNavigate, ...props }: GuardedLinkProps) {
+export function GuardedLink({ href, onNavigate, ...props }: GuardedLinkProps) {
+  const router = useRouter()
   const { guardNavigation } = useDirtyFormGuard()
 
   return (
     <Link
       {...props}
+      href={href}
       onNavigate={(event) => {
         onNavigate?.(event)
-        if (event.defaultPrevented) return
-
         event.preventDefault()
+
         guardNavigation(() => {
-          window.location.assign(event.currentTarget.href)
+          router.push(href.toString())
         })
       }}
     />
